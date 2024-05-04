30839d5c6bc67aff72d77d18f19cf471790a127c
Abhinav Palugula <99081921+Palugula-Abhinav@users.noreply.github.com>
2024-05-04 21:35:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener("scroll", function () {
   var navbar = document.getElementById("navbar");
   var scrollThreshold = 40; // Adjust this value to set the scroll threshold
@@ -38,25 +37,4 @@ darkmode.onclick = () => {
     document.body.classList.remove("active");
   }
 };
-=======
-// Get a reference to the navbar element
-const navbar = document.querySelector('#navbar');
 
-// Define the scroll threshold at which you want to change the navbar styling
-const scrollThreshold = 80; // Adjust this value as needed
-
-// Function to toggle the 'scrolled' class on the navbar based on scroll position
-function toggleNavbarStyling() {
-    if (window.scrollY >= scrollThreshold) {
-        navbar.classList.add('scrooled');
-    } else {
-        navbar.classList.remove('scrooled');
-    }
-}
-
-// Add a scroll event listener to the window
-window.addEventListener('scroll', toggleNavbarStyling);
-
-// Initial call to set the navbar styling based on the initial scroll position
-toggleNavbarStyling();
->>>>>>> 53cf1d4564ddfbbefe9060e77b8d98c3d6df3c7a
